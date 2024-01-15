36d4657afc58ab58d35bafcb40eaade4a0b4304f
wellington miguel <wellington2010_silva@hotmail.com>
2024-01-15 16:09:15 -0300

    add response api pokedex in js file

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
 let offiset = 0;
 let limit = 10;
 let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offiset}`
-console.log(url);
+function addPokemonLi (pokemon){
+    return `<li class="pokemon">
+    <span id="number">#0001</span>
+    <span>${pokemon.name}</span>
+    <div>
+        <p>Tipo:</p>
+        <img id="pokemon-img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="pokemon">
+    </div>
+</li>`
+}
+ fetch(url)
+ .then((response)=>response.json())
+ .then((jsonResponse)=>jsonResponse.results)
+.then((pokemons)=>{
+    for(i= 0; i<=pokemons.length; i++){
+        const pokemon = pokemons[i]
+        const pokemonLi = document.getElementById(`pokemonOl`)
+        pokemonLi.innerHTML += addPokemonLi(pokemon)
+    }
+})
